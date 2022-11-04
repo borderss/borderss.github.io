30e62f1c0a3e02a0eacce2046a3118e154fc8b7a
Alberts Liepins <liepins01@gmail.com>
2022-11-04 19:03:16 +0200

    minor parameter optimization

--- src/react/board.jsx
+++ src/react/board.jsx
@@ -10,50 +10,29 @@ function board(props) {
       target.setAttribute("style", `box-shadow: 0px 20px 100px 0px #252525; background-color: var(--gray); outline: 2px dashed var(--board-outline); transform: translateY(5px); transition: box-shadow 0.5s, transform 0.5s`)
       return
     }
-    
-    if (e.target.classList.contains("card-container")) {
-      target = e.target.parentNode
-
-      target.setAttribute("style", `box-shadow: 0px 20px 100px 0px #252525; background-color: var(--gray); outline: 2px dashed var(--board-outline); transform: translateY(5px); transition: box-shadow 0.5s, transform 0.5s`)
-    }
   }
 
-  const DragLeave = (e) => {  
+  const DragLeave = () => {  
     target.style.removeProperty('outline')
     target.setAttribute("style", `background-color: var(--gray); transition: box-shadow 0.5s, transform 0.5s`)
   }
 
-  const DragEnd = (e) => {
+  const DragEnd = () => {
     document.querySelectorAll(".board").forEach(board => {
       board.style.removeProperty('outline')
       board.setAttribute("style", `background-color: var(--gray); transition: box-shadow 0.5s, transform 0.5s`)
     })
-    document.querySelectorAll(".card").forEach(card => {
-      card.style.removeProperty('outline')
-      card.style.removeProperty('transform')
-    })
   }
 
-  let cardsData
-
-  if (props.data.length != 0) {
-    cardsData = props.data
-
-  } else {
-    cardsData = (
-      <div className={"empty-card-hint"}>
-        Drop a card here!
-      </div>
-    )
-  }
+  let cardsData = props.data.length != 0 ? props.data : (
+    <div className={"empty-card-hint"}>
+      Drop a card here!
+    </div>
+  )
 
   document.querySelectorAll(".board").forEach(board => {
     board.style.removeProperty('outline')
     board.setAttribute("style", `background-color: var(--gray); transition: box-shadow 0.5s, transform 0.5s`)
-  })
-  document.querySelectorAll(".card").forEach(card => {
-    card.style.removeProperty('outline')
-    card.style.removeProperty('transform')
   })
 
   return (
@@ -63,8 +42,8 @@ function board(props) {
     onDragOver={props.dragOver} 
     onDrop={props.onDrop}
     onDragEnter={e => DragEnter(e)}
-    onDragLeave={e => DragLeave(e)}
-    onDragEnd={e => DragEnd(e)}
+    onDragLeave={_ => DragLeave()}
+    onDragEnd={_ => DragEnd()}
     >
       <h2>{props.title}</h2>
       <div className={"card-container"}>
--- src/react/boards.jsx
+++ src/react/boards.jsx
@@ -86,89 +86,6 @@ export default function Boards() {
     }
   )
 
-  const fakeFetch = () => {
-    return {
-      backlog: [
-        {
-          title: "Twilio integration",
-          desc: "Create new note via SMS. Support text, audio, links, and media.",
-          color: "pink",
-        },
-        {
-          title: "Markdown support",
-          desc: "Markdown shorthand converts to formatting",
-          tag: "Formatting",
-          color: "purple",
-        },
-      ],
-      todo: [
-        {
-          title: "Audio recording in note",
-          desc: "Show audio in a note and playback UI",
-          tag: "Note interface",
-          color: "purple",
-        },
-        {
-          title: "Tablet view",
-          color: "red",
-        },
-        {
-          title: "Bookmark in note",
-          desc: "Show rich link UI in a note, and feature to render website screenshot",
-          tag: "Note interface",
-          color: "purple",
-        },
-        {
-          title: "Image viewer",
-          desc: "Opens when clicking on the thumbnail in the list or on the image in the note",
-          color: "purple",
-        },
-      ],
-      inprogress: [
-        {
-          title: "Mobile view",
-          desc: "Functions for both web responsive and native apps. Note: Android and iOS will need unique share icons",
-          color: "red",
-        },
-        {
-          title: "Desktop view",
-          desc: "PWA for webiste and native apps. Note: Windows and Mac will need unique share icons.",
-          color: "red",
-        },
-        {
-          title: "Formatting options",
-          desc: "Mobile formatting bar expands and collapses when tapping the format icon.",
-          color: "blue",
-        },
-        {
-          title: "Media in note",
-          desc: "Image & video with player UI",
-          tag: "Note interface",
-          color: "purple",
-        },
-      ],
-      designed: [
-        {
-          title: "Audio recording",
-          desc: "Interface for when recording a new audio note",
-          tag: "New note",
-          color: "green",
-        },
-        {
-          title: "Bookmarking",
-          desc: "Interface for when creating a new link note.",
-          tag: "New note",
-          color: "green",
-        },
-        {
-          title: "Mobile home screen",
-          desc: "Folders, tags, and notes lists are sorted by recent.",
-          color: "blue",
-        },
-      ],
-    }
-  }
-
   const genCards = (board, boardName) => {
     const output = new Array()
 
@@ -196,12 +113,17 @@ export default function Boards() {
     const numRegex = /([\d]+)/
 
     let cardKey = e.dataTransfer.getData("key")
-    let currData = Object.assign({}, data)
+    if (cardKey == "") {
+      useForceRefresh()
+      return
+    }
 
     let originBoard = strRegex.exec(cardKey)[0]
     let originId = numRegex.exec(cardKey)[0]
-    let entry = currData[originBoard].splice(originId, 1)[0]
 
+    let currData = Object.assign({}, data)
+    let entry = currData[originBoard].splice(originId, 1)[0]
+    
     let targetBoard = e.target.closest(".board").id
 
 
@@ -214,14 +136,33 @@ export default function Boards() {
     setData(currData)
   }
 
-  // setData(fakeFetch())
-
   return (
     <div className={"boards"}>
-      <Board id={"backlog"} title={"Backlog"} data={genCards(data.backlog, "backlog")} dragOver={e=>handleDragOver(e)} onDrop={e=>handleCardDrop(e)}/>
-      <Board id={"todo"} title={"To do"} data={genCards(data.todo, "todo")} dragOver={e=>handleDragOver(e)} onDrop={e=>handleCardDrop(e)}/>
-      <Board id={"inprogress"} title={"In progress"} data={genCards(data.inprogress, "inprogress")} dragOver={e=>handleDragOver(e)} onDrop={e=>handleCardDrop(e)}/>
-      <Board id={"designed"} title={"Designed"} data={genCards(data.designed, "designed")} dragOver={e=>handleDragOver(e)} onDrop={e=>handleCardDrop(e)}/>
+      <Board 
+      id={"backlog"} 
+      title={"Backlog"} 
+      data={genCards(data.backlog, "backlog")} 
+      dragOver={e=>handleDragOver(e)} 
+      onDrop={e=>handleCardDrop(e)}/>
+
+      <Board 
+      id={"todo"} 
+      title={"To do"} 
+      data={genCards(data.todo, "todo")} 
+      dragOver={e=>handleDragOver(e)} 
+      onDrop={e=>handleCardDrop(e)}/>
+
+      <Board id={"inprogress"} 
+      title={"In progress"} 
+      data={genCards(data.inprogress, "inprogress")} 
+      dragOver={e=>handleDragOver(e)} 
+      onDrop={e=>handleCardDrop(e)}/>
+
+      <Board id={"designed"} 
+      title={"Designed"}
+      data={genCards(data.designed, "designed")} 
+      dragOver={e=>handleDragOver(e)} 
+      onDrop={e=>handleCardDrop(e)}/>
     </div>
   )
 }
