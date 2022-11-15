017937e3d4b7eb56b141b5d285a279720eb41f11
Alberts Liepins <liepins01@gmail.com>
2022-11-15 23:43:31 +0200

    API actually completed

--- api/app/Http/Controllers/Api/LabelController.php
+++ api/app/Http/Controllers/Api/LabelController.php
@@ -10,58 +10,28 @@ use Illuminate\Http\Request;
 
 class LabelController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
   public function index()
   {
     return LabelResource::collection(Label::all());
   }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
   public function store(LabelRequest $request)
   {
     $new_label = Label::create($request->validated());
     return new LabelResource($new_label);
   }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function show(Label $label)
   {
     return new LabelResource($label);
   }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function update(LabelRequest $request, Label $label)
   {
     $label->update($request->validated());
     return new LabelResource($label);
   }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function destroy(Label $label)
   {
     $label->delete();
