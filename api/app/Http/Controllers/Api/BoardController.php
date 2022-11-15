6d20bf743595f76fd6d3fbb5c19b52e3499fed25
Alberts Liepins <liepins01@gmail.com>
2022-11-15 23:43:54 +0200

    API actually completed

--- api/app/Http/Controllers/Api/BoardController.php
+++ api/app/Http/Controllers/Api/BoardController.php
@@ -10,58 +10,28 @@ use Illuminate\Http\Request;
 
 class BoardController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
   public function index()
   {
     return BoardResource::collection(Board::all());
   }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
   public function store(BoardRequest $request)
   {
     $new_board = Board::create($request->validated());
     return new BoardResource($new_board);
   }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function show(Board $board)
   {
     return new BoardResource($board);
   }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function update(BoardRequest $request, Board $board)
   {
     $board->update($request->validated());
     return new BoardResource($board);
   }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function destroy(Board $board)
   {
     $board->delete();
